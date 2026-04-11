c11775d83f74f7b7b5730de95d81af3351daa070
Parth Parikh <parthparikh1999p@gmail.com>
2026-04-11 02:59:56 -0700

    Fix auto mute

--- tools/mute.py
+++ tools/mute.py
@@ -2,7 +2,7 @@ DEFINITION = {
     "type": "function",
     "function": {
         "name": "mute",
-        "description": "Call this EVERY time the user says thank you, goodbye, see you later, that's all, or otherwise signals they are done. Also call it when you answer a one-off question that needs no follow-up. You will go to sleep and the user must say the wake word to talk again.",
+        "description": "Go to sleep. Call this when the user says thanks, goodbye, nothing, or is done talking.",
         "parameters": {
             "type": "object",
             "properties": {},
